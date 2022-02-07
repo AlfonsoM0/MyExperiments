dca6217e5877caf065b8047a458efaa46ff701ea
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-02-06 21:04:14 -0300

    En proceso

--- logica.js
+++ logica.js
@@ -141,3 +141,50 @@ console.log(h);
 // celia: 3
 // rosa: 2
 
+/* 
+Words and letters
+Given a 'bag' of words and a 'bag' of letters, write a
+function that returns the length of the longest word that
+can be written using each of the given letters at most
+once.
+e.g. 
+Words:
+------------------------------
+| 'kellogg' 'go'       |
+|'hola' 'lego' 'hug'|
+| 'kocomo' 'hello' |
+------------------------------
+Letters:
+-----------------------
+| 'a' 'h' 'l' 'e' |
+|'l' 'o' 'g' 'k'  |
+-----------------------
+Answer: 5
+Explanation:
+* 5 is the length of 'hello', logest one that can be written
+* 'kellogg' (7) needs two 'g', and there's only one in the letters bag
+* 'kocomo' (6) needs a 'c' that is not present in the letters bag
+* All other words are shorter than 'hello'
+Bonus:
+1. What's the Time complexity?
+2. What's the Space complexity?
+*/
+
+let words = ["kellogg", "go", "hola", "lego", "hug", "kocomo", "hello"];
+let letters = ["a", "h", "l", "e", "l", "o", "g", "k"];
+
+function wordsAndLetters(words, letters) {
+  words = words.sort((a, b) => b.length - a.length);
+  
+  for (let w = 0; w < words.length; w++) {
+    for (let l = 0; l < words[w].length; l++) {
+      
+      
+    }
+    
+  }
+  
+}
+
+wordsAndLetters(words, letters);
+
