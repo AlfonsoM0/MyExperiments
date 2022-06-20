7f2ef808253776565345439759bfddfc96da2845
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-06-20 18:55:10 -0300

    Funny banner for linkedin

---
+++ varios/funnybanner.js
@@ -0,0 +1,35 @@
+'use strict';
+const Candidates = [];
+
+const skills = [
+  'Full Stack Web Developer',
+  'React',
+  'Redux',
+  'PostgreSQL',
+  'Express',
+  'Node',
+  'NPM',
+  'Business Administration',
+  'Leadership',
+  'Creativity',
+  'Problem Solving',
+  'Curiosity',
+  'Teaching Experience',
+  'Professional Consulting Experience',
+  'Sales Experience',
+];
+
+function getBestEmployee(Candidates) {
+  let whatYouNeed;
+
+  //#region selection
+
+  const selection = 'Alfonso Montes de Oca';
+  Candidates = true;
+  if (Candidates) return selection;
+
+  //#endregion
+
+  if (whatYouNeed) return selection;
+}
+console.log(getBestEmployee(Candidates));
